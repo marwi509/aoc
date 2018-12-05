26f6b00c98fb995911d6d58e134375b63b09ea51
Marcus Widegren <marcus.widegren@qapital.com>
2018-12-05 11:00:39 +0100

    Some cleanup

--- src/main/java/aoc/Fifth.java
+++ src/main/java/aoc/Fifth.java
@@ -1,6 +1,7 @@
 package aoc;
 
 import aoc.util.FileReader;
+import org.apache.commons.lang.time.StopWatch;
 
 import java.util.HashSet;
 import java.util.Objects;
@@ -10,44 +11,44 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Fifth {
 
     private static final String alphabet = "abcdefghijklmnopqrstuvqxyz";
+    private static final StopWatch sw = new StopWatch();
 
     public static void main(String[] args) {
+        sw.start();
         Set<Integer> lengths = new HashSet<>();
         String line = new FileReader().readFile("input.fifth").get(0);
 
         String original = line;
-        for(char c : alphabet.toCharArray()) {
-            String upperCase = (""+c).toUpperCase();
-            String lowerCase = (""+c).toLowerCase();
+        for (char c : alphabet.toCharArray()) {
+            StringBuilder sb = new StringBuilder();
+            String upperCase = ("" + c).toUpperCase();
+            String lowerCase = ("" + c).toLowerCase();
             line = original.replace(lowerCase, "").replace(upperCase, "");
-            lengths.add(getResult(line, original));
+            sb.append(line);
+            lengths.add(getResult(sb));
         }
 
         System.out.println(lengths.stream().mapToInt(i -> i).min().orElseThrow());
+        System.out.println(sw.getTime());
     }
 
-    private static int getResult(String line, String original) {
+    private static int getResult(StringBuilder line) {
         AtomicInteger index = new AtomicInteger(0);
-        while(index.get() < line.length() - 1) {
+        while (index.get() < line.length() - 1) {
             line = checkForMatch(line, index);
             index.incrementAndGet();
         }
 
-        System.out.println(line.length() + " " + original.length());
         return line.length();
     }
 
-    private static String checkForMatch(String line, AtomicInteger currentIndex) {
+    private static StringBuilder checkForMatch(StringBuilder line, AtomicInteger currentIndex) {
         if (currentIndex.get() < 0 || currentIndex.get() >= line.length() - 1) {
             return line;
         }
-        if (Objects.equals(("" + line.charAt(currentIndex.get())).toLowerCase(),  (""+line.charAt(currentIndex.get() + 1)).toLowerCase()) &&
-                !Objects.equals(("" + line.charAt(currentIndex.get())),  (""+line.charAt(currentIndex.get() + 1)))) {
-            if (currentIndex.get() <= line.length() - 2) {
-                line = line.substring(0, currentIndex.get()) + line.substring(currentIndex.get() + 2);
-            } else {
-                line = line.substring(0, currentIndex.get());
-            }
+        if (Objects.equals(("" + line.charAt(currentIndex.get())).toLowerCase(), ("" + line.charAt(currentIndex.get() + 1)).toLowerCase()) &&
+                !Objects.equals(("" + line.charAt(currentIndex.get())), ("" + line.charAt(currentIndex.get() + 1)))) {
+            line = line.replace(currentIndex.get(), currentIndex.get() + 2, "");//line.substring(0, currentIndex.get()) + line.substring(currentIndex.get() + 2);
             currentIndex.decrementAndGet();
             return checkForMatch(line, currentIndex);
         }
